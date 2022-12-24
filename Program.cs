c854725a96f8633a8bc570e121862921ada0e004
Mahmoudashraf05 <121326380+Mahmoudashraf05@users.noreply.github.com>
2022-12-24 05:59:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,2 @@
-﻿Console.WriteLine("Please enter the starting number of range");
-int startingNumber = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Please enter the ending number of range");
-int endingNumber = int.Parse(Console.ReadLine());
-
-Console.WriteLine("The prime numbers between " + startingNumber + " and " + endingNumber + " are");
-
-for (int i = startingNumber; i <= endingNumber; i++)
-{
-    int count = 0;
-
-    for (int l = 2; l <= i / 2; l++)
-    {
-        if (i % l == 0)
-        {
-            count++;
-            break;
-        }
-    }
-    if (count == 0 && i != 0)
-    {
-        Console.WriteLine(i);
-    }
-}
-Console.ReadKey();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
